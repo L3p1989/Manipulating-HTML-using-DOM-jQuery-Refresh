62f6b9dac67f43145741af416f007728157a5d2b
L3p1989 <phogan110@live.com>
2021-03-08 15:26:14 -0700

    created variables for #txtBox, and #sbmtBtn. created click function for sbmtBtn

--- jQuery/scripts.js
+++ jQuery/scripts.js
@@ -3,3 +3,9 @@ const btnOne = $('<button>Click Me!</button>').click(() => {
 });
 const body = $('body');
 body.append(btnOne);
+const txtBox = $('#txtBox');
+const sbmtBtn = $('#sbmtBtn');
+sbmtBtn.click(() => {
+    txtBox.val() == '' ? alert("You haven't typed anything!") : alert(txtBox.val());
+    txtBox.val('')
+});
